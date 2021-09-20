c5032f10e149404a7f0c0612297dacecad5e82ec
Youcef Aouali <you.aouali@gmail.com>
2021-09-20 17:16:45 +0200

    2nd COMMIT, travail sur les differents menus

--- projet_P4/vues/vue_player.py
+++ projet_P4/vues/vue_player.py
@@ -3,23 +3,30 @@
 from ..modeles.modele_player import Player
 
 
-class VuePlayer:
-
-    def __call__(self):
-        return self.create_player_input()
+class PlayerView:
 
     @staticmethod
     def create_player_input():
-
         """ Input for create player & return inputs """
+        print("Ajouter un nouveau joueur :  ")
+        print()
+
 
         f_name = input('Prénom : ')
         name = input('Nom : ')
-        birthdate = input('Date de naissance (DD-MM-YYYY): ')
         genre = input('genre (M ou F) : ')
+        birthdate = input('Date de naissance (DD-MM-YYYY): ')
         age = input("Âge ? ")
         score = input('Score : ')
         ranking = input('Classement: ')
 
-        Player(f_name, name, birthdate, genre, age, score, ranking)
+        Player(f_name, name, birthdate, genre, age, score, ranking).create_players()
+
+
+    @staticmethod
+    def show_player(player):
+        print(f'{player["name"]} - {player["f_name"]},  Classement : {player["ranking"]}')
+
+
+
 
